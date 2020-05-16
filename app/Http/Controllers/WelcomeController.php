c956d91f6f9b6ae83d388be9016f02863a261788
Krystian <krystian@bpcoders.pl>
2020-05-16 12:55:49 +0200

    add proper order

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\DB;
 class WelcomeController extends Controller
 {
     public function index() {
-        $images = Gif::all();
+        $images = Gif::orderBy('id', 'DESC')->get();
         return view('welcome')->with('images', $images);
     }
 
